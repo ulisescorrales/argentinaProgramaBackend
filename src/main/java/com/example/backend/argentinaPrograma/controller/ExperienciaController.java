9dfbbb33efb11bb3f0305a6c384365771abbda28
ulisescorrales <ulisescorralesallen@gmail.com>
2022-12-23 01:40:00 -0300

    implementado peticiones tecnologia

--- src/main/java/com/example/backend/argentinaPrograma/controller/ExperienciaController.java
+++ src/main/java/com/example/backend/argentinaPrograma/controller/ExperienciaController.java
@@ -9,6 +9,7 @@ import com.example.backend.argentinaPrograma.service.IExperienciaService;
 import java.sql.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Corrales Ulises
  */
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ExperienciaController {
 
