62471ee4543a3f260b1201f953100792f007ecf8
ulisescorrales <ulisescorralesallen@gmail.com>
2022-12-27 17:42:41 -0300

    primera implementacion security

---
+++ src/main/java/com/example/backend/argentinaPrograma/security/Entity/Rol.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.backend.argentinaPrograma.security.Entity;
+
+import com.example.backend.argentinaPrograma.security.Enums.RolNombre;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author Corrales Ulises
+ */
+@Entity
+@Getter @Setter
+public class Rol {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RolNombre rolNombre;
+    
+    //Constructor
+
+    public Rol() {
+    }
+
+    public Rol(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+    
+}
