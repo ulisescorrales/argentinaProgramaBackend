140b8516f417828c39debcbe2e622814d5456d9f
ulisescorrales <ulisescorralesallen@gmail.com>
2022-12-26 23:22:00 -0300

    agregado dependencias para implementar JWT

---
+++ src/main/java/com/example/backend/argentinaPrograma/controller/AuthController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.backend.argentinaPrograma.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Corrales Ulises
+ */
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:4200")
+public class AuthController {
+    
+}
