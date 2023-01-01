5b6d35d8968b7fc314054210749688ba9bfb8051
ulisescorrales <ulisescorralesallen@gmail.com>
2023-01-01 18:06:07 -0300

    segundo intento

--- src/main/java/com/example/backend/argentinaPrograma/WebConfig.java
+++ src/main/java/com/example/backend/argentinaPrograma/WebConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200","https://argentinaprogramabackend-n3d0.onrender.com")
+                            .allowedMethods("*");
     }
 }
