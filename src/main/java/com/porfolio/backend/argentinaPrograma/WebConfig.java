1a4c5b073ba585e0b6ac5ca83de8ddaf0ccd5dd7
ulisescorrales <ulisescorralesallen@gmail.com>
2023-11-26 14:37:05 -0300

    modificado PersonaController

--- src/main/java/com/porfolio/backend/argentinaPrograma/WebConfig.java
+++ src/main/java/com/porfolio/backend/argentinaPrograma/WebConfig.java
@@ -18,7 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://ulises-corrales-ap.web.app/")
+        registry.addMapping("/**").allowedOrigins("https://ulises-corrales-ap.web.app/","*")
                             .allowedMethods("*");        
     }
 }
